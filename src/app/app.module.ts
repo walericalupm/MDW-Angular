6cb3e753bb5991b313e8303d54cc89586a02f3f2
Sandra Ortega <sandita.saan@gmail.com>
2019-04-23 21:33:27 +0200

    Generate detail-room component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomDetailComponent } from './room-detail/room-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomDetailComponent
   ],
   imports: [
     BrowserModule,
